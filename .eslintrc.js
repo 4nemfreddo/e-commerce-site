22d9fe2e41965547fd6daba0886ab40a3feb7409
4nemfreddo <killmyenvy@gmail.com>
2024-03-21 00:24:59 +0300

    added fonts and functionality that will be useful for later updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
+const { EndOfLineState } = require('typescript')
+
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 }
